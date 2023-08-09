604ae433d39d3cafb2d8c1fd9e03ee408804b925
Dave <102557749+KaluDavid@users.noreply.github.com>
2023-08-09 17:10:12 +0100

    Mobile commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("visible");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
