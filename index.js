f68eaf6a5315f244788f7a427bdb1782d2e711c1
Dave <102557749+KaluDavid@users.noreply.github.com>
2023-08-11 06:06:06 +0100

    script commit

--- index.js
+++ index.js
@@ -6,3 +6,54 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+
+
+  // Hide and show div
+  let cuisines = document.getElementById('cuisines');
+  let event1 = document.getElementById('event');
+  let gears = document.getElementById('gears');
+  let sites = document.getElementById('site');
+
+const items = [cuisines, event1, gears, sites];
+for(let content of items){
+  if(content == sites){
+    content.style.display = 'flex';
+  }
+  else{
+    content.style.display = 'none';
+  }
+}
+
+//butons for call
+let bt1 = document.getElementById('bt1');
+let bt2 = document.getElementById('bt2');
+let bt3 = document.getElementById('bt3');
+let bt4 = document.getElementById('bt4');
+
+bt1.addEventListener('click', function(){
+  event1.style.display = 'none';
+  gears.style.display = 'none';
+  sites.style.display = 'none';
+  cuisines.style.display ='flex';
+})
+bt2.addEventListener('click', function(){
+  event1.style.display = 'none';
+  gears.style.display = 'none';
+  sites.style.display = 'flex';
+  cuisines.style.display ='none';
+})
+bt3.addEventListener('click', function(){
+  event1.style.display = 'flex';
+  gears.style.display = 'none';
+  sites.style.display = 'none';
+  cuisines.style.display ='none';
+})
+bt4.addEventListener('click', function(){
+  event1.style.display = 'none';
+  gears.style.display = 'flex';
+  sites.style.display = 'none';
+  cuisines.style.display ='none';
+})
+
+
